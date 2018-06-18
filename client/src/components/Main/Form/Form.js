e47d9eeeb89bc9cd7f0eee79e7c3f39e479685c7
Poladashili <ge0rge9.git@gmail.com>
2018-06-18 09:27:34 +0300

    get data info from form user

--- client/src/components/Main/Form/Form.js
+++ client/src/components/Main/Form/Form.js
@@ -6,28 +6,29 @@ import Input from './Input';
 class FormControl extends Component {
   constructor(props){
     super(props);
-    this.state={
+    this.state = {
       first:'',surname:'',middle:'',email:'',gender:'',age:'',photo:''
     };
-    this.handlerInput=this.handlerInput.bind(this);
-    FormControl.handlerSelect=FormControl.handlerSelect.bind(this);
+    this.clickRegister = this.clickRegister.bind(this);
+    this.handlerInput = this.handlerInput.bind(this);
+    FormControl.handlerSelect = FormControl.handlerSelect.bind(this);
   }
 
   static handlerInputFile(e){
-    const elem=e.target.parentNode.nextSibling;
-    elem.innerText='Photo upload';
-    elem.style.backgroundColor='#2185d0';
+    const elem = e.target.parentNode.nextSibling;
+    elem.innerText = 'Photo upload';
+    elem.style.backgroundColor = '#2185d0';
     console.log(e.target.parentNode.nextSibling);
   }
   static validate(regExp,name,value){
     return regExp[name].test(value);
   }
   static validateName(regExp,name,value){
-    return (value.search(regExp.name)!== -1);
+    return (value.search(regExp.name) !== - 1);
   }
   static handlerSelect(e){
     if(e.target.children[0].innerText){
-      this.setState({gender: e.target.children[0].innerText});
+      this.setState({gender:e.target.children[0].innerText});
       e.target.classList.remove('err');
     }else{
       this.setState({gender:e.target.children[0].innerText});
@@ -36,11 +37,12 @@ class FormControl extends Component {
   }
 
   handlerInput(e){
-    const {type,value,name}=e.target;
-    if(type==='text'&&name!=='age'){
+    const {type,value,name} = e.target;
+    if(type === 'text' && name !== 'age'){
       if(FormControl.validateName(regExp,name,value)){
         this.setState({[name]:value});
         e.target.classList.remove('err');
+        //console.log(this.state)
       }else{
         e.target.classList.add('err');
       }
@@ -54,88 +56,98 @@ class FormControl extends Component {
     }
   }
 
+  clickRegister(){
+    Object.keys(this.state).filter(field =>{
+
+      if(this.state[field]){
+        console.log('Field ready', this.state[field])
+      } else{
+        console.log('Field not ready', this.state[field]);
+      }
+
+    });
+  };
+
   render(){
     const {configLang} = this.props;
-    const options=[{
+    const options = [{
       key:'m',text:configLang.gender.male,value:'male'
     },{
       key:'f',text:configLang.gender.female,value:'female'
     }];
-
-    const clickRegister = () => {
-      console.log(this.state)
-    };
-    const onPhotoChange = () => {
+    const onPhotoChange = () =>{
       const file = this.fileUpload.files[0];
-
-      if(file.size > 40 && file.size < 5000) {
+      if(file.size > 40 && file.size < 5000){
         console.log('file dow')
-      } else{
+      }else{
         console.log('file err size')
       }
-
       console.log(file);
     };
-
     return (<div className='FormBox'>
-        <Form className='Form' size='mini'>
-          <Input
-            label={configLang.name}
-            name='first'
-            placeHolder={configLang.name}
-            onChange={this.handlerInput}
+      <Form className='Form' size='mini'>
+        <Input
+          label={configLang.name}
+          name='first'
+          placeHolder={configLang.name}
+          onChange={this.handlerInput}
+        />
+        <Input
+          name='surname'
+          label={configLang.surname}
+          placeHolder={configLang.surname}
+          onChange={this.handlerInput}
+        />
+        <Form.Group widths={2}>
+          <Form.Field
+            control={Select}
+            name='gender'
+            required={true}
+            label={configLang.gender.title}
+            options={options}
+            onChange={FormControl.handlerSelect}
+            placeholder={configLang.gender.title}
           />
           <Input
-            name='surname'
-            label={configLang.surname}
-            placeHolder={configLang.surname}
+            name='age'
+            type='number'
+            label={configLang.age}
+            placeHolder={configLang.age}
             onChange={this.handlerInput}
           />
-          <Form.Group widths={2}>
-            <Form.Field
-              control={Select}
-              name='gender'
-              required={true}
-              label={configLang.gender.title}
-              options={options}
-              onChange={FormControl.handlerSelect}
-              placeholder={configLang.gender.title}
-            />
-            <Input
-              name='age'
-              type='number'
-              label={configLang.age}
-              placeHolder={configLang.age}
-              onChange={this.handlerInput}
-            />
-          </Form.Group>
-          <Input
-            name='middle'
-            required={false}
-            label={configLang.middle}
-            placeHolder={configLang.middle}
-            onChange={this.handlerInput}
-          />
-          <Input
-            name='email'
-            type='email'
-            label={configLang.email}
-            placeHolder={configLang.email}
-            onChange={this.handlerInput}
-          />
-          <div className='buttonBox'>
-            <Button fluid icon='download' className='dowLand'/>
-            <input type="file" onChange={onPhotoChange} ref={(ref) => this.fileUpload = ref}/>
-          </div>
-          <Button
-            fluid color='blue'
-            size='small'
-            onClick={clickRegister}
-          >
-            {configLang.button}
-          </Button>
-        </Form>
-      </div>)
+        </Form.Group>
+        <Input
+          name='middle'
+          required={false}
+          label={configLang.middle}
+          placeHolder={configLang.middle}
+          onChange={this.handlerInput}
+        />
+        <Input
+          name='email'
+          type='email'
+          label={configLang.email}
+          placeHolder={configLang.email}
+          onChange={this.handlerInput}
+        />
+      </Form>
+      <form className='buttonBox' encType="multipart/form-data">
+        <Button fluid icon='download' className='dowLand'/>
+        <input
+          type="file"
+          onChange={onPhotoChange}
+          ref={(ref) => this.fileUpload = ref}
+          accept=".png, .jpg, .jpeg"
+        />
+      </form>
+      <Button
+        fluid color='blue'
+        size='small'
+        onClick={this.clickRegister}
+      >
+        {configLang.button}
+      </Button>
+    </div>)
   }
 }
 
