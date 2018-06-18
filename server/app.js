c363a9c1f0eee91179e5d0d92bc07402c69bfc37
Poladashili <ge0rge9.git@gmail.com>
2018-06-18 17:10:22 +0300

    send form to server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require( 'express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
+const bodyParser = require('body-parser');
 const CONST = require( './constants' );
 const Schema = require('./model/User/userSchema');
 
@@ -9,9 +11,14 @@ const app = express();
 const db = mongoose.connection;
 const { PORT, URL_DB } = CONST;
 const { User } = Schema;
+const urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.use(cors());
+app.use( bodyParser.json() );
+app.use('/static', express.static(__dirname + '/public'));
+
 mongoose.connect(URL_DB);
+
 db.on('error', console.error.bind(console, 'Connection db error:'));
 db.once('open', () => {
   console.log('Connect db');
@@ -36,4 +43,8 @@ app.get('/api/users', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`listening on port ${ PORT }`);
+});
+
+app.post("/user", urlencodedParser, (request, response) => {
+  response.send(response.data);
 });
